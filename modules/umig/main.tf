5b430cf3355869245aefaeb2b0be61f7505ecfb4
Giuseppe Palermo <gpalermo@enter.eu>
2023-05-24 16:37:39 +0200

    add healthcheck creation in umig module https

--- modules/umig/main.tf
+++ modules/umig/main.tf
@@ -125,6 +125,25 @@ output "num_instances" {
     value = var.num_instances
 }
 
+resource "google_compute_health_check" "https" {
+  count   = var.health_check["type"] == "https" ? 1 : 0
+  project = var.project_id
+  name    = var.health_check_name == "" ? "${var.hostname}-https-healthcheck" : var.health_check_name
+
+  check_interval_sec  = var.health_check["check_interval_sec"]
+  healthy_threshold   = var.health_check["healthy_threshold"]
+  timeout_sec         = var.health_check["timeout_sec"]
+  unhealthy_threshold = var.health_check["unhealthy_threshold"]
+
+  https_health_check {
+    port         = var.health_check["port"]
+    request_path = var.health_check["request_path"]
+    host         = var.health_check["host"]
+    response     = var.health_check["response"]
+    proxy_header = var.health_check["proxy_header"]
+  }
+}
+
 resource "google_compute_region_health_check" "http" {
   count   = var.health_check["type"] == "http" ? 1 : 0
   project = var.project_id
